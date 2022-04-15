9699a9ab10255f377fa31e23bb93a68841cd4596
favour-olumese <70489864+favour-olumese@users.noreply.github.com>
2022-04-15 23:39:23 +0100

    Greatest Common Divisor
    Day 11: Euclid's algorithm (GCD).
    The GCD (Greatest Common divisor) of two numbers is the largest number by which both are divisible. E.g.; gcd(42, 18) is 6, since 6 is the highest common factor (HCF—same thing as GCD) of 42 and 18.
    
    Task:
    Write a program that asks the user for two numbers and computes their GCD using Euclid's algorithm. The process is described below:
    * First, compute the remainder of dividing the larger number by the smaller one.
    * Next, replace the larger number with the smaller number, and the smaller number with the remainder.
    * Repeat this process until the smaller number equals zero. The GCD is the last value of the larger number.
    (See full details: https://en.m.wikipedia.org/wiki/Euclidean_algorithm#:~:text=In%20mathematics%2C%20the%20Euclidean%20algorithm,them%20both%20without%20a%20remainder.

---
+++ day11_greatest_common_divisor.py
@@ -0,0 +1,46 @@
+# ECX 30 Days of Code and Design
+# Day 11
+"""
+**Euclid's algorithm (GCD).**
+
+The GCD (Greatest Common divisor) of two numbers is the largest number by which both are divisible.
+E.g.; gcd(42, 18) is 6, since 6 is the highest common factor (HCF—same thing as GCD) of 42 and 18.
+
+Task:\n
+Write a program that asks the user for two numbers and computes their GCD using Euclid's algorithm.
+The process is described below:
+* First, compute the remainder of dividing the larger number by the smaller one.
+* Next, replace the larger number with the smaller number, and the smaller number with the remainder.
+* Repeat this process until the smaller number equals zero. The GCD is the last value of the larger number.
+(See full details:
+https://en.m.wikipedia.org/wiki/Euclidean_algorithm
+#:~:text=In%20mathematics%2C%20the%20Euclidean%20algorithm,them%20both%20without%20a%20remainder.
+"""
+
+
+# Greatest common divisor function
+def euclidean(big_num, small_num):
+    # Make the first argument the lager of both arguments
+    if small_num > big_num:
+        temp = big_num
+        big_num = small_num
+        small_num = temp
+
+    # Keep dividing until the remainder is 0
+    while small_num > 0:
+        # Dummy variable to hold the value of the smallest number
+        remainder = small_num
+        small_num = big_num % small_num
+        big_num = remainder
+
+    print('The greatest common divisor is', big_num)
+
+
+# Ask users for input
+print(' Greatest Common Divisor Finder '.center(40, '*'))
+print('Enter two positive integer')
+first_num = int(input('First number: '))
+second_num = int(input('Second number: '))
+
+# Function call
+euclidean(first_num, second_num)
