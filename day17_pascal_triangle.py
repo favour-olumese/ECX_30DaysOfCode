a10ccefb1c02c4d78f8bcbc861f71cbfd7bda6c5
favour-olumese <70489864+favour-olumese@users.noreply.github.com>
2022-05-09 05:47:59 +0100

    Pascal Triangle
    * Write a function that prints out the first "n" rows of Pascal's triangle.
    Where "n" is an integer taken as argument of the function.

---
+++ day17_pascal_triangle.py
@@ -0,0 +1,30 @@
+# ECX 30 DAYS OF CODE AND DESIGN
+# Day 17
+
+"""
+**Pascal triangle**
+
+Task: \n
+* Write a function that prints out the first "n" rows of Pascal's triangle.
+Where "n" is an integer taken as argument of the function.
+[See more](https://en.m.wikipedia.org/wiki/Pascal%27s_triangle)
+"""
+import math                 # For comb() [Combination] function
+
+
+def pascal_triangle(numbers):
+    """Prints Pascal Triangle Using Combination"""
+
+    for num_of_elements in range(numbers + 1):
+        for selected_elements in range(num_of_elements + 1):
+            print(math.comb(num_of_elements, selected_elements), end=' ')
+
+        print('\n')
+
+
+print(' Pascal Triangle '.center(40, '*'))
+try:
+    number = int(input('Enter the range of Pascal triangle to print: '))
+    pascal_triangle(number)
+except ValueError:
+    print('Invalid input. Enter an integer.')
