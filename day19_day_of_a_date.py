c623fcfa24b69d0410756e0f1d4b04a9239d2bc8
favour-olumese <70489864+favour-olumese@users.noreply.github.com>
2022-05-09 05:50:31 +0100

    Day of a Date
    Write a function that takes in a date as input, and returns what day of the week it is.
    * The input date can be in any convenient format(Whether a "ddmmyy" string, a series of integers, etc)
    * Your function must work for both future and past dates.
    * Exception handling(or Type checking) is necessary.

---
+++ day19_day_of_a_date.py
@@ -0,0 +1,32 @@
+# ECX 30 DAYS OF CODE AND DESIGN
+# Day 19
+
+"""
+Task: \n
+Write a function that takes in a date as input, and returns what day of the week it is.
+* The input date can be in any convenient format(Whether a "ddmmyy" string, a series of integers, etc)
+* Your function must work for both future and past dates.
+* Exception handling(or Type checking) is necessary.
+"""
+
+import datetime             # For datetime(), strftime()
+
+
+def day_of_the_date():
+    """Prints out the day of a given date."""
+    try:
+        print(' Get the day name of a particular date '.center(45, '*'))
+        year = int(input('Year; 2011: '))
+        month = int(input('Month; Jan - 1, Feb - 2 ...: '))
+        day = int(input('Day; 1 - (28...31): '))
+
+        date = datetime.datetime(year, month, day)
+        print(date.strftime('%Y-%m-%d') + ' is a ' + date.strftime('%A'))
+        return date.strftime('%A')
+
+        # To take care of a case where the user input a wrong month/day number
+    except ValueError:
+        print('Invalid! Please, input the correct month/day range.')
+
+
+day_of_the_date()
