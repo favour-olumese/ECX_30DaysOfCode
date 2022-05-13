0d782e29063c8a32bfe5ca22bf2abbbe040f0e3d
favour-olumese <70489864+favour-olumese@users.noreply.github.com>
2022-05-13 07:47:38 +0100

    Wordle Game
    * The user makes a first guess (e.g., "Skate").
    * Print out a progress guide, like this; "√××√+".
    * "√" Indicates that the letter at that position was guessed correctly.
    * "+" indicates that the letter at that position is in the hidden word, but in a different position.
    * "×" indicates that the letter at that position is wrong, and isn't in the hidden word.
    * This process is repeated until the user either guesses the hidden word correctly—in which case, he wins—or exhausts his 6 attempts; thus, losing.
    * The "hidden word" is generated randomly from a list of 5-letter words hard-coded by you.

--- day7_wordle.py
+++ day7_wordle.py
@@ -8,7 +8,7 @@
 + indicates letter is in the word, but wrong position.
 x indicates that the letter is not in the word.
 """
-import random
+import random               # For randint
 
 word_dict = ['bread', 'broke', 'brace', 'boost', 'mouse', 'loose', 'crust', 'books', 'loops', 'sings', 'bloat',
              'broad', 'chair', 'table', 'seats', 'apple', 'brown', 'brook', 'phone', 'brush', 'jeans', 'clean',
@@ -26,9 +26,9 @@ word_dict_span = len(word_dict)
 
 word = word_dict[random.randint(0, word_dict_span - 1)]
 
-print('Wordle Game')
-print('/ indicates correct letter and position.')
-print('+ indicates letter is in the word, but wrong position.')
+print(' Wordle Game '.center(40, '*'))
+print('/ indicates the correct letter and position.')
+print('+ indicates the letter is in the word, but in the wrong position.')
 print('x indicates that the letter is not in the word.\n')
 
 print('You are to input a 5 letter word. You have 6 tries.')
@@ -36,19 +36,28 @@ print('You are to input a 5 letter word. You have 6 tries.')
 for i in range(6):
     count = 0
     player_input = input('Enter the word: ').lower()
-    for j in player_input:
-        if j in word:
-            if word[count] == j:
-                print('/', end='')
+
+    if len(player_input) == 5:
+        for j in player_input:
+            if j in word:
+                if word[count] == j:
+                    print('/', end='')
+                else:
+                    print('+', end='')
             else:
-                print('+', end='')
-        else:
-            print('x', end='')
+                print('x', end='')
 
-        count = count + 1
+            count = count + 1
 
-    if player_input == word:
-        print('\nYou won')
-        break
+        if player_input == word:
+            print('\nYou won')
+            break
 
-    print('\n')
+        if i == 5 and player_input != word:
+            print('\nSorry, you lost. The word is', word)
+
+        print('\n')
+
+    else:
+        print('Invalid entry! Enter a five letter word.')
+        continue
