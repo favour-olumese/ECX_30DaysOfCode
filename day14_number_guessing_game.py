927d7bfd1c44526e1c3c789633061900ecce4aa7
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-06-18 02:11:13 +0100

    Guess the Number
    You are to ask a user to guess a number between 1 and 50.
    The user has a maximum of 5 tries.
    If the user guesses wrongly, provide an error message indicating whether their guess was above or below the actual number.
    If the user guesses correctly, congratulate them and show the number of attempts they had.
    If the user exhausts all their tries, tell them they have exhausted their tries and end the game.
    E.g.:
    > Enter a number
    user: 1
    > Wrong. the answer is greater than 1
    user: 25
    > Wrong the answer is less than 25
    user: 14
    > Wrong the number is greater than 14
    user: 15
    > Correct! You got the right answer in 3 tries.

--- day14_number_guessing_game.py
+++ day14_number_guessing_game.py
@@ -1,23 +1,23 @@
-# ECX 30 DAYS OF CODE
+# ECX 30 DAYS OF CODE AND DESIGN
 # Day 14
 
 """
 **Guess the number**
 
-Task: \n 
+Task: \n
 You are to ask a user to guess a number between 1 and 50.
 The user has a maximum of 5 tries.
 If the user guesses wrongly, provide an error message indicating whether their guess was above or below the actual number.
 If the user guesses correctly, congratulate them and show the number of attempts they had.
 If the user exhausts all their tries, tell them they have exhausted their tries and end the game.
-E.G:
-> enter a number
+E.g.:
+> Enter a number
 user: 1
-> wrong. the answer is greater than 1
+> Wrong. the answer is greater than 1
 user: 25
-> wrong the answer is less than 25
+> Wrong the answer is less than 25
 user: 14
-> wrong the number is greater than 14
+> Wrong the number is greater than 14
 user: 15
 > Correct! You got the right answer in 3 tries.
 """
@@ -31,15 +31,17 @@ print('You have 5 tries to get the answer.')
 print('Guess the number from 1 to 50.')
 
 for i in range(5):
-    player_guess = int(input('Guess the number: '))
+    try:
+        player_guess = int(input('Guess the number: '))
 
-    if player_guess < random_num:
-        print('Wrong: The answer is greater than', player_guess)
-    elif player_guess > random_num:
-        print('Wrong: The answer is less than', player_guess)
-    elif player_guess == random_num:
-        print('Correct! You got the right answer in ' + str(i + 1) + ' tries')
-        break
-
+        if player_guess < random_num:
+            print('Wrong: The answer is greater than', player_guess)
+        elif player_guess > random_num:
+            print('Wrong: The answer is less than', player_guess)
+        elif player_guess == random_num:
+            print('Correct! You got the right answer in ' + str(i + 1) + ' tries')
+            break
+    except ValueError:
+        print('Invalid input! Input only integers.')
 if player_guess != random_num:
     print('Sorry! The answer is', random_num)
