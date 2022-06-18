5f25c347fb4a0101174e7beafcf9c34a02ace36f
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-06-18 02:19:55 +0100

    Day of a Date
    Write a function that takes in a date as input, and returns what day of the week it is.
    * The input date can be in any convenient format(Whether a "ddmmyy" string, a series of integers, etc)
    * Your function must work for both future and past dates.
    * Exception handling (or Type checking) is necessary.

--- day19_day_of_a_date.py
+++ day19_day_of_a_date.py
@@ -6,7 +6,7 @@ Task: \n
 Write a function that takes in a date as input, and returns what day of the week it is.
 * The input date can be in any convenient format(Whether a "ddmmyy" string, a series of integers, etc)
 * Your function must work for both future and past dates.
-* Exception handling(or Type checking) is necessary.
+* Exception handling (or Type checking) is necessary.
 """
 
 import datetime             # For datetime(), strftime()
@@ -24,7 +24,7 @@ def day_of_the_date():
         print(date.strftime('%Y-%m-%d') + ' is a ' + date.strftime('%A'))
         return date.strftime('%A')
 
-        # To take care of a case where the user input a wrong month/day number
+        # To take care of a case where the user inputs a wrong month/day number
     except ValueError:
         print('Invalid! Please, input the correct month/day range.')
 
