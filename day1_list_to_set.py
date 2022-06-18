4abe9ac92159dfcc3f97d8435d08a4b9770dff9f
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-06-18 01:42:17 +0100

    List to Set
    Create a function that takes in a list as input, and returns (prints) a new list with all repetitions reduced to one appearance alone, as output.

--- day1_list_to_set.py
+++ day1_list_to_set.py
@@ -1,23 +1,19 @@
-# ECX 30 DAYS OF CODE
-
+# ECX 30 DAYS OF CODE AND DESIGN
 # Day 1
-
-"""Create a function that takes in a list as input, and returns (prints) a new list with all repetitions reduced to one appearance alone, as output."""
-
+"""
+Create a function that takes in a list as input, and returns (prints) a new list
+with all repetitions reduced to one appearance alone, as output.
+"""
 # List
-
 ecx_items = ["a", "b", "a", "a", 3, 3, 2, "hello", "b"]
 
-def set_function(list_items):   
 
-  """Takes a list and prints a new list with repetitions removed"""
+def set_function(list_items):
+    """Takes a list and prints a new list with repetitions removed"""
+    set_list = set(list_items)
 
-  set_list = set(list_items)
+    print(list(set_list))
 
-  print(list(set_list))
-
-  
 
 # Function call
-
 set_function(ecx_items)
